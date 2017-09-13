42b7a59c4e34fe16edae7c3093549114d246d667
Clare Hsu <clare.hsu390@gmail.com>
2017-09-12 17:00:05 -0700

    Complete CRUD for session

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,7 +11,7 @@ class Api::UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        render 'api/users.show'
+        render 'api/users/show'
     end
 
     private
