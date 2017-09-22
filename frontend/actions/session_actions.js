17ac188f68976d2dc4c7b13dce4be77f3e94232c
Clare Hsu <clare.hsu390@gmail.com>
2017-09-22 13:33:47 -0700

    Add fail callback for session errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,23 +1,23 @@
 import * as APIUtil from '../util/session_api_util';
-
+import { receiveErrors, clearErrors } from './error_actions';
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 
 export const login = user => dispatch => {
     return APIUtil.login(user).then(
         currentUser => dispatch(receiveCurrentUser(currentUser))
-    );
+    ).fail(errors => dispatch(receiveErrors(errors.responseJSON)));
 };
 
 export const signup = user => dispatch => {
     return APIUtil.signup(user).then(
         currentUser => dispatch(receiveCurrentUser(currentUser))
-    );
+    ).fail(errors => dispatch(receiveErrors(errors.responseJSON)));
 };
 
 export const logout = () => dispatch => {
     return APIUtil.logout().then(
         user => dispatch(receiveCurrentUser(null))
-    );
+    ).fail(errors => dispatch(receiveErrors(errors)));
 };
 
 export const receiveCurrentUser = user => ({
