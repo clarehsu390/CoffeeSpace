1e9f30f86217f4599dcd50631f720bc504cf810d
Clare Hsu <clare.hsu390@gmail.com>
2017-09-27 20:15:07 -0700

    Add handlesubmit to session form

--- frontend/components/session_form/session_form.js
+++ frontend/components/session_form/session_form.js
@@ -7,5 +7,13 @@ class SessionForm extends React.Component {
             username: "",
             password: ""
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
+
 }
