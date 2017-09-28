e32771d0a67bc1b3368742a6f2a86686e10ea19c
Clare Hsu <clare.hsu390@gmail.com>
2017-09-27 20:04:53 -0700

    Fixed store

--- frontend/coffeespace.jsx
+++ frontend/coffeespace.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { login } from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<Root store={ store }/>, root);
     const store = configureStore();
+    ReactDOM.render(<Root store={ store }/>, root);
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     window.login = login;
