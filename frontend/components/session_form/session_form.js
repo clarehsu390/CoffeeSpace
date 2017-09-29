35bd93ddaaed95cb2893470f93ed3aebe7171c3a
Clare Hsu <clare.hsu390@gmail.com>
2017-09-29 13:25:37 -0700

    Complete login form

--- frontend/components/session_form/session_form.js
+++ frontend/components/session_form/session_form.js
@@ -17,21 +17,30 @@ class SessionForm extends React.Component {
         this.props.processForm(user);
     }
 
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
     render() {
         return (
             <div>
+                <form onSubmit={this.handleSubmit} className="login-form">
                 <label>Username:
                 <input type="text"
                 value={this.state.username}
-               /> 
+                onChange={this.update('username')}/> 
                 </label>
 
                 <label>Password:
-                    <input type="text"
+                    <input type="password"
                     value={this.state.password}
-                     /> 
+                    onChange={this.update('password')}/> 
 
                 </label>
+                <input type="submit" value="Submit"/>
+            </form>
             </div>
         );
     }
