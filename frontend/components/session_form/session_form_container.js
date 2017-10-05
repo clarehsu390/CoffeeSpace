ba6a9e26b58703541a6bdc74beceb57eeeb8113c
Clare Hsu <clare.hsu390@gmail.com>
2017-10-04 22:31:08 -0700

    Style session errors

--- frontend/components/session_form/session_form_container.js
+++ frontend/components/session_form/session_form_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { signup, login } from '../../actions/session_actions';
-import SessionForm from './session_form.js';
+import SessionForm from './session_form';
 
 const mapStateToProps = (state) => ({
     loggedIn: Boolean(state.currentUser),
