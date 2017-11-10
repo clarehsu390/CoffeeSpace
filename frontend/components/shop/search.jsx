88a145413e69cd440b9bc39cb2512be5c2afca86
Clare Hsu <clare.hsu390@gmail.com>
2017-11-09 16:52:44 -0800

    show coffee shops near current location

--- frontend/components/shop/search.jsx
+++ frontend/components/shop/search.jsx
@@ -8,19 +8,22 @@ export default class Search extends React.Component {
    componentDidMount() {
        let input = document.getElementById('search');
         this.search = new google.maps.places.SearchBox(input);
+        
    }
 
    componentDidUpdate() {
+    this.props.map.addListener('bounds_changed', () => {
+        this.search.setBounds(this.props.map.getBounds());        
+    });
        this.search.addListener('places_changed', () => {
            let places = this.search.getPlaces();
            let markers = [];
-
            markers.forEach((marker) => {
                marker.setMap(null);
            });
 
            let bounds = new google.maps.LatLngBounds();
-
+        
            places.forEach((place) => {
                console.log(place);
                let icon = {
