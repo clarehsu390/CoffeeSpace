c9b9aaa07f8568949a8005aec431593b74e8fcd0
Clare Hsu <clare.hsu390@gmail.com>
2017-11-09 16:11:32 -0800

    Adjust viewport

--- frontend/components/shop/search.jsx
+++ frontend/components/shop/search.jsx
@@ -8,38 +8,46 @@ export default class Search extends React.Component {
    componentDidMount() {
        let input = document.getElementById('search');
         this.search = new google.maps.places.SearchBox(input);
-        this.search.addListener('places_changed', () => {
-            let places = this.search.getPlaces();
-
-            if (places.length === 0) {
-                return;
-            }
-            let markers = [];
-
-            markers.forEach((marker) => {
-                marker.setMap(null);
-            });
-
-            let bounds = new google.maps.LatLngBounds();
-
-            places.forEach((place) => {
-                let icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                  };
-            });
-
-            markers.push(new google.maps.Marker({
-                map: this.props.map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-        });
    }
+
+   componentDidUpdate() {
+       this.search.addListener('places_changed', () => {
+           let places = this.search.getPlaces();
+           let markers = [];
+
+           markers.forEach((marker) => {
+               marker.setMap(null);
+           });
+
+           let bounds = new google.maps.LatLngBounds();
+
+           places.forEach((place) => {
+               console.log(place);
+               let icon = {
+                   url: place.icon,
+                   size: new google.maps.Size(71, 71),
+                   origin: new google.maps.Point(0, 0),
+                   anchor: new google.maps.Point(17, 34),
+                   scaledSize: new google.maps.Size(25, 25)
+                 };
+
+           markers.push(new google.maps.Marker({
+               map: this.props.map,
+               icon: icon,
+               title: place.name,
+               position: place.geometry.location
+           }));
+
+           if (place.geometry.viewport) {
+               bounds.union(place.geometry.viewport);
+           } else {
+               bounds.extend(place.geometry.location);
+           }
+       });
+       this.props.map.fitBounds(bounds);
+    });
+   }
+   
 
    render() {
        return (
